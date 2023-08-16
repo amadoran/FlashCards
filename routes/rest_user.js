9384944132de713e674bb2a0b9c7e8c751109d35
amadoran <afamador@espol.edu.ec>
2023-08-16 13:34:00 -0500

    guia 22

--- routes/rest_user.js
+++ routes/rest_user.js
@@ -1,36 +1,95 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* IMPORTE El ARCHIVO CON EL NOMBRE_CLASE */
-const user = require('../models').user;
+const user = require("../models").user;
 
-router.get('/findAll/json', function(req, res, next) {  
-
+router.get("/findAll/json", function (req, res, next) {
     /* MÉTODO ESTÁTICO findAll  */
 
-  user.findAll({  
-      attributes: { exclude: ["updatedAt", "createdAt"] } ,
-  })  
-  .then(resultado => {  
-      res.json(resultado);  
-  })  
-  .catch(error => res.status(400).send(error)) 
-
+    user.findAll({
+        attributes: { exclude: ["updatedAt", "createdAt"] },
+    })
+        .then((resultado) => {
+            res.json(resultado);
+        })
+        .catch((error) => res.status(400).send(error));
 });
 
-   router.get('/findById/:id/json', function(req, res, next) {  
+router.get("/findById/:id/json", function (req, res, next) {
+    let id = req.params.id;
 
-      let id = req.params.id;
-
-      user.findByPk(id)
-          .then(instancia => {
-            if(instancia) {
-              res.status(200).json(instancia);
+    user.findByPk(id)
+        .then((instancia) => {
+            if (instancia) {
+                res.status(200).json(instancia);
             } else {
-              res.status(404).json({error: "No existe registro con el identificador "+id})
+                res.status(404).json({
+                    error: "No existe registro con el identificador " + id,
+                });
             }
-          })
-          .catch(error => res.status(400).send(error))
-    });
-  
+        })
+        .catch((error) => res.status(400).send(error));
+});
+
+router.post("/save", function (req, res, next) {
+    user.create(req.body)
+        .then((instancia) => {
+            res.status(201).json(instancia);
+        })
+        .catch((error) => {
+            res.status(500).json({ error: "Error al crear el registro" });
+        });
+});
+
+router.put("/update/:id", function (req, res, next) {
+    let id = req.params.id;
+
+    user.findByPk(id)
+        .then((instancia) => {
+            if (instancia) {
+                instancia
+                    .update(req.body)
+                    .then((instanciaActualizada) => {
+                        res.status(201).json(instanciaActualizada);
+                    })
+                    .catch((error) => {
+                        res.status(500).json({
+                            error: "Error al actualizar el registro",
+                        });
+                    });
+            } else {
+                res.status(404).json({
+                    error: "No existe registro con el identificador " + id,
+                });
+            }
+        })
+        .catch((error) => res.status(400).send(error));
+});
+
+router.delete("/delete/:id", function (req, res, next) {
+    let id = req.params.id;
+
+    user.findByPk(id)
+        .then((instancia) => {
+            if (instancia) {
+                instancia
+                    .destroy()
+                    .then(() => {
+                        res.status(204).json({ mensaje: "Registro eliminado" });
+                    })
+                    .catch((error) => {
+                        res.status(500).json({
+                            error: "Error al actualizar el registro",
+                        });
+                    });
+            } else {
+                res.status(404).json({
+                    error: "No existe registro con el identificador " + id,
+                });
+            }
+        })
+        .catch((error) => res.status(400).send(error));
+});
+
 module.exports = router;
