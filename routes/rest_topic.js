d61a88f3be13ed5efe4d118e0af7295b11849ba4
amadoran <afamador@espol.edu.ec>
2023-09-06 11:00:31 -0500

    eliminar topic no ver el codigo porfavor

--- routes/rest_topic.js
+++ routes/rest_topic.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 /* IMPORTE El ARCHIVO CON EL NOMBRE_CLASE */
 const topic = require("../models").topic;
+const flashcard_topic = require("../models").flashcard_topic;
+const flashcard = require("../models").flashcard;
 
 router.get("/findAll/json", function (req, res, next) {
     /* MÉTODO ESTÁTICO findAll  */
@@ -20,7 +22,8 @@ router.get("/findAll/json", function (req, res, next) {
 router.get("/findById/:id/json", function (req, res, next) {
     let id = req.params.id;
 
-    topic.findByPk(id)
+    topic
+        .findByPk(id)
         .then((instancia) => {
             if (instancia) {
                 res.status(200).json(instancia);
@@ -34,7 +37,8 @@ router.get("/findById/:id/json", function (req, res, next) {
 });
 
 router.post("/save", function (req, res, next) {
-    topic.create(req.body)
+    topic
+        .create(req.body)
         .then((instancia) => {
             res.status(201).json(instancia);
         })
@@ -46,7 +50,8 @@ router.post("/save", function (req, res, next) {
 router.put("/update/:id", function (req, res, next) {
     let id = req.params.id;
 
-    topic.findByPk(id)
+    topic
+        .findByPk(id)
         .then((instancia) => {
             if (instancia) {
                 instancia
@@ -71,18 +76,45 @@ router.put("/update/:id", function (req, res, next) {
 router.delete("/delete/:id", function (req, res, next) {
     let id = req.params.id;
 
-    topic.findByPk(id)
+    topic
+        .findByPk(id)
         .then((instancia) => {
             if (instancia) {
-                instancia
-                    .destroy()
-                    .then(() => {
-                        res.status(204).json({ mensaje: "Registro eliminado" });
+                flashcard_topic
+                    .findAll({
+                        attributes: { exclude: ["updatedAt", "createdAt"] },
+                        where: { topic_id: id },
                     })
-                    .catch((error) => {
-                        res.status(500).json({
-                            error: "Error al actualizar el registro",
-                        });
+                    .then((resultado) => {
+                        if (resultado) {
+                            if (resultado.length > 0) {
+                                for (let registro of resultado) {
+                                    registro.destroy().then(() => {
+                                        flashcard
+                                            .destroy({
+                                                where: {
+                                                    id: registro.flashcard_id,
+                                                },
+                                            })
+                                            .then(() =>
+                                                instancia
+                                                    .destroy()
+                                                    .then(() => {
+                                                        res.status(204).json({
+                                                            mensaje:
+                                                                "Registro eliminado",
+                                                        });
+                                                    })
+                                                    .catch((error) => {
+                                                        res.status(500).json({
+                                                            error: error,
+                                                        });
+                                                    })
+                                            );
+                                    });
+                                }
+                            }
+                        }
                     });
             } else {
                 res.status(404).json({
